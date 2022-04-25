8a932fbbd10f24f64636de1b97d1a741779a75cb
pavancool <etukalapavan41@gmail.com>
2022-04-25 15:43:28 +0530

    added a new maven project

---
+++ src/main/java/accentureTechnologies/App.java
@@ -0,0 +1,13 @@
+package accentureTechnologies;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
